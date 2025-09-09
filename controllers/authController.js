047d16d9969bb32e516f54ff706006dea7386ee8
ThakurAbhay9457 <143515821+ThakurAbhay9457@users.noreply.github.com>
2025-09-10 01:51:38 +0530

    some files updated

--- controllers/authController.js
+++ controllers/authController.js
@@ -103,3 +103,69 @@ export const logout = async (req, res)=> {
     }
 }
 
+// send verification otp to the user's email
+export const sendVerifyOtp = async (req, res)=> {
+    try {
+        const {userId} = req.user;
+
+        const user = await userModel.findById(userId);
+
+        if(user.isAccountVerified){
+            return res.json({success: false, message: "Account already verified"})
+        }
+
+        const otp = String(Math.floor(100000 + Math.random() * 900000)); 
+
+        user.verifyOtp = otp;
+        user.verifyOtpExpireAt = Date.now() + 24 * 60 * 60 * 1000;
+
+        await user.save();
+
+        const mailOption = {
+            from: process.env.SENDER_EMAIL,
+            to: user.email,
+            subject: "Account verification OTP",
+            text: `Your OTP is ${otp}. Verify your account using this OTP.`,
+            
+        }
+        await transporter.sendMail(mailOption);
+
+        res.json({success: true, message: "OTP sent successfully"});
+
+    } catch (error) {
+        res.json({success: false, message: error.message});
+    }
+}
+
+export const verifyEmail = async(req, res)=> {
+    const {userId, otp} = req.body;
+
+    if(!userId || !otp){
+        return res.json({success: false, message: 'Missing Details'});
+    }
+    try {
+        const user = await userModel.findById(userId);
+
+        if(!user){
+            return res.json({ success: false, message: 'User not found'});
+        }
+
+        if(user.verifyOtp === '' || user.verifyOtp !== otp){
+            return res.json({success: false, message: 'Invalid otp'});
+        }
+
+        if(user.verifyOtpExpireAt < Date.now()){
+            res.json({success: false, message: 'OTP expired'});
+        }
+
+        user.isAccountVerified = true;
+        user.verifyOtp = '';
+        user.verifyOtpExpireAt = 0;
+
+        await user.save();
+        return res.json({success: true, message: 'Account verified successfully'});
+
+    }catch (error) {
+        return res.json({success: false, message: error.message});
+    }
+}
