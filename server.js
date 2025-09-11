a712687ac5de14f0db229d01050da36cc706bc43
ThakurAbhay9457 <143515821+ThakurAbhay9457@users.noreply.github.com>
2025-09-11 12:17:06 +0530

    added some changes

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import cookieParser from "cookie-parser";
 import authRouter from './routes/authRoutes.js';
 
 import connectDB from './config/mongodb.js'
+import userRouter from './routes/userRoutes.js'
 
 const app = express();
 const port = process.env.PORT || 4000
@@ -19,5 +20,6 @@ app.use(cors({credentials: true}));
 // API endpoints
 app.get('/', (req, res)=> res.send("API Working"));
 app.use('/api/auth', authRouter);
+app.use('/api/user', userRouter);
 
 app.listen(port, ()=> console.log(`Server is running on port ${port}`));
