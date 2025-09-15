c2e78b8dbd66da7ff71f0ac097481e06ab9fd63e
ThakurAbhay9457 <143515821+ThakurAbhay9457@users.noreply.github.com>
2025-09-15 20:10:05 +0530

    merged

---
+++ middleware/userAuth.js
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken";
+
+const userAuth = async (req, res, next) => {
+  const { token } = req.cookies;
+
+  if (!token) {
+    return res.json({ success: false, message: "Unauthorized" });
+  }
+
+  try {
+    const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (tokenDecode.id) {
+      req.user = { userId: tokenDecode.id }; //  attach user info safely
+    } else {
+      return res.json({ success: false, message: "Unauthorized" });
+    }
+
+    next();
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+
+export default userAuth;
