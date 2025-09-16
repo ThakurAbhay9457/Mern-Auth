9f95d74693c3fef27f9f35b8b452850aa9f63b45
ThakurAbhay9457 <143515821+ThakurAbhay9457@users.noreply.github.com>
2025-09-16 20:39:29 +0530

    updates

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,4 +1,5 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
+import { toast } from 'react-toastify';
 
 export const AppContent = createContext()
 
@@ -8,14 +9,30 @@ export const AppContextProvider = (props)=>{
     const [isLoggedin, setIsLoggedin] = useState(false)
     const [userData, setUserData] = useState(false)
 
+    const getAuthState = async()=> {
+        try {
+            const {data} = await axios.get(backendUrl + '/api/auth/is-auth')
+            if(data.success){
+                setIsLoggedIn(true)
+                getUserData()
+            }
+        } catch (error) {
+            toast.error(error.response.data.message);
+        }
+    }
+
     const getUserData = async ()=>{
         try {
             const {data} = await axios.get(backendUrl + '/api/user/data')
             data.success ? setUserData(data.userData) : toast.error(data.message)
         } catch (error) {
-            toast.error(data.message)
+            toast.error(error.message)
         }
     }
+
+    useEffect(()=>{
+        getAuthState();
+    },[])
 
     const value = {
         backendUrl,
